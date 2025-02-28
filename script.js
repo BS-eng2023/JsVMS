f0c69052531484268cb82a5c79a9e7e118b77d0d
BS-eng2023 <azaam.mahmood@mail.de>
2025-02-28 17:00:56 +0100

    last  update  28.02.2025

--- script.js
+++ script.js
@@ -44,6 +44,42 @@
                 let right= document.querySelector(".right");
                 right.classList.toggle("rightSearchClicked");
             });
+
+            fetch('carList02.json')
+            .then(response => response.json())
+            .then(data => {
+                carsData = data;
+                //populateFilters();
+                displayCars(data);
+            })
+            .catch(error => console.error('Error loading the JSON file:', error));
+
+        function displayCars(cars) {
+            const container = document.querySelector('.main');
+            container.innerHTML = '';
+            cars.forEach(car => {
+                const carElement = document.createElement('div');
+                carElement.className = 'car-listing';
+                carElement.innerHTML = `
+                <h2>${car.title}</h2>
+                    <div class="car-container">
+                        <img src="${car.previewImage}"  class="car-image">
+                        <div class="car-info">
+                            <p><strong>Price:</strong> ${car.price.total.localized}</p>
+                            <p><strong>Brand:</strong> ${car.brand} | <strong>Model:</strong> ${car.model}</p>
+                            <p><strong>Category:</strong> ${car.category}</p>
+                            <p><strong>Mileage:</strong> ${car.attributes["Mileage"]}</p>
+                            <p><strong>Fuel:</strong> ${car.attributes["Fuel"]}</p>
+                            <p><a href="${car.url}" target="_blank">View Listing</a></p>
+                        </div>
+                    </div>
+                `;
+                container.appendChild(carElement);
+
+
+            });
+        }
+
         }
 
         // INIT
