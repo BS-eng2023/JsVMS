82af1d0c3cc00fafcb89f45a4fcd380ec3b9de35
BS-eng2023 <azaam.mahmood@mail.de>
2025-03-06 16:07:22 +0100

    login is working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,91 +1,88 @@
-            "use strict";
+"use strict";
 
-            //// add car script
-            import elements from './elements.js';
-            import addCar from './addCar.js';
-            import addUser from './addUser.js';
-            import logIn from './logIn.js';
-            //import populateFilters from "./components/populateFilters.js";
-            import displayCars from "./components/displayCars.js";
-            import applyFilters from "./components/applyFilters.js";
+//// add car script
+import elements from "./elements.js";
+import addCar from "./addCar.js";
+import addUser from "./addUser.js";
+import logIn from "./logIn.js";
+//import populateFilters from "./components/populateFilters.js";
+import displayCars from "./components/displayCars.js";
+import applyFilters from "./components/applyFilters.js";
+import login from "./logIn.js";
 
-            //disable add new car button //
-            const addNewCarButton = document.getElementById("addNewCar");
-            addNewCarButton.title = "Bitte zuerst anmelden!";
-            addNewCarButton.disabled = true;
+//disable add new car button //
+const addNewCarButton = document.getElementById("addNewCar");
+addNewCarButton.title = "Bitte zuerst anmelden!";
+addNewCarButton.disabled = true;
 
-            const domMapping = () => {
-                 elements.main = document.querySelector('.main');
-                 elements.main.innerHTML="";
-                 elements.logIn = document.querySelector('#logIn');
-                 elements.addNewCar = document.querySelector('#addNewCar');
-                 elements.addNewUser = document.querySelector('#addNewUser');
+const domMapping = () => {
+  elements.main = document.querySelector(".main");
+  elements.main.innerHTML = "";
+  elements.logIn = document.querySelector("#logIn");
+  elements.addNewCar = document.querySelector("#addNewCar");
+  elements.addNewUser = document.querySelector("#addNewUser");
+};
 
-           }
+const appendEventListeners = () => {
+  elements.logIn.addEventListener("click", logIn);
+  elements.addNewCar.addEventListener("click", addCar);
+  elements.addNewUser.addEventListener("click", addUser);
+};
 
-            const appendEventListeners = () => {
-                elements.logIn.addEventListener('click', logIn);
-                elements.addNewCar.addEventListener('click', addCar);
-                elements.addNewUser.addEventListener('click', addUser);
-            }
-            
-         
-            function populateFilters(carsData) {
-                const brandFilter = document.getElementById('brand-filter');
-                const fuelFilter = document.getElementById('fuel-filter');
-                
-                const brands = new Set();
-                const fuels = new Set();
-                
-                carsData.forEach(car => {
-                    brands.add(car.attributes ["Marken"]);
-                    fuels.add(car.attributes["Kraftstoffe :"]);
-                });
-                
-                brands.forEach(brand => {
-                    const option = document.createElement('option');
-                    option.value = brand;
-                    option.textContent = brand;
-                    brandFilter.appendChild(option);
-                });
-                
-                fuels.forEach(fuel => {
-                    const option = document.createElement('option');
-                    option.value = fuel;
-                    option.textContent = fuel;
-                    fuelFilter.appendChild(option);
-                });
-            }
+function populateFilters(carsData) {
+  const brandFilter = document.getElementById("brand-filter");
+  const fuelFilter = document.getElementById("fuel-filter");
 
-            const init = () => {
-            //add car script
-            domMapping();
-            appendEventListeners();
-            //
+  const brands = new Set();
+  const fuels = new Set();
 
-             fetch('../assets/json/sample_cars.json')
-            .then(response => response.json())
-            .then(data => {
-                elements.data=data;
-                populateFilters(data);
-                displayCars(data);
-            })
-            .catch(error => console.error('Error loading the JSON file:', error));
-            
-            //to display Filter bar on the right side
-            const buttonDisplayFilter = document.querySelector("#displayfilter");
-            buttonDisplayFilter.addEventListener("click", function () {
-                let right = document.querySelector(".right");
-                right.classList.toggle("rightSearchClicked");
-            });
-             //to apply Filter 
-             const buttonApllyFilter = document.querySelector(".applyFilters");
-             buttonApllyFilter.addEventListener("click", function () {
-                applyFilters(elements.data);
-             });
+  carsData.forEach((car) => {
+    brands.add(car.attributes["Marken"]);
+    fuels.add(car.attributes["Kraftstoffe :"]);
+  });
 
-           
-            };
+  brands.forEach((brand) => {
+    const option = document.createElement("option");
+    option.value = brand;
+    option.textContent = brand;
+    brandFilter.appendChild(option);
+  });
 
-            // INIT
-            document.addEventListener("DOMContentLoaded", init);
+  fuels.forEach((fuel) => {
+    const option = document.createElement("option");
+    option.value = fuel;
+    option.textContent = fuel;
+    fuelFilter.appendChild(option);
+  });
+}
+
+const init = () => {
+  //add car script
+  domMapping();
+  appendEventListeners();
+  login();
+  
+  fetch("../assets/json/sample_cars.json")
+    .then((response) => response.json())
+    .then((data) => {
+      elements.data = data;
+      populateFilters(data);
+      displayCars(data);
+    })
+    .catch((error) => console.error("Error loading the JSON file:", error));
+
+  //to display Filter bar on the right side
+  const buttonDisplayFilter = document.querySelector("#displayfilter");
+  buttonDisplayFilter.addEventListener("click", function () {
+    let right = document.querySelector(".right");
+    right.classList.toggle("rightSearchClicked");
+  });
+  //to apply Filter
+  const buttonApllyFilter = document.querySelector(".applyFilters");
+  buttonApllyFilter.addEventListener("click", function () {
+    applyFilters(elements.data);
+  });
+};
+
+// INIT
+document.addEventListener("DOMContentLoaded", init);
--- assets/js/logIn.js
+++ assets/js/logIn.js
@@ -1,74 +1,94 @@
 "use strict";
 import elements from "./elements.js";
 import createInputField from "./components/inputData.js";
+import golbalData from "./components/golbalData.js";
 
 const login = () => {
-    elements.main.innerHTML = "";
-    const elSelction = document.createElement("h2");
-    elSelction.innerHTML = "Anmelden";
-    elements.main.append(elSelction);
-    const formElement  = document.createElement("form");
-    formElement.className = 'userForm'; 
-    elements.main.appendChild(formElement);
-    createInputField(formElement, "Benutzer Name:","userName","text","benutzername");
-    createInputField(formElement, "Passwort:","userPassword","password","password");
+  elements.main.innerHTML = "";
+  console.log(elements);
+  const elSelction = document.createElement("h2");
+  elSelction.innerHTML = "Anmelden";
+  elements.main.append(elSelction);
+  const formElement = document.createElement("form");
+  formElement.className = "userForm";
+  elements.main.appendChild(formElement);
+  createInputField(
+    formElement,
+    "Benutzer Name:",
+    "userName",
+    "text",
+    "benutzername"
+  );
+  createInputField(
+    formElement,
+    "Passwort:",
+    "userPassword",
+    "password",
+    "password"
+  );
 
-    const loginButton = document.createElement("button");
-    loginButton.className = 'userLogin'; 
-    loginButton.style.fontSize = "18px"; // Set font size
-    //Button.onclick = saveCarData; // Assign function
+  const loginButton = document.createElement("button");
+  loginButton.className = "userLogin";
+  loginButton.style.fontSize = "18px"; // Set font size
 
-    // Create icon element
-    const icon = document.createElement("i");
-    icon.classList.add("fas", "fa-sign-in"); // Add FontAwesome classes
+  // Create icon element
+  const icon = document.createElement("i");
+  icon.classList.add("fas", "fa-sign-in"); // Add FontAwesome classes
 
-    // Set button text
-    loginButton.appendChild(icon);
-    loginButton.appendChild(document.createTextNode("  Anmelden"));
-    elements.main.appendChild(loginButton);
+  // Set button text
+  loginButton.appendChild(icon);
+  loginButton.appendChild(document.createTextNode("  Anmelden"));
+  elements.main.appendChild(loginButton);
 
+  const addNewCarButton = document.getElementById("addNewCar");
+  const currentName = document.querySelector("#userName");
+  const currentPassword = document.querySelector("#userPassword");
 
-    const logInButton = document.getElementById("logIn");
-    const addNewCarButton = document.getElementById("addNewCar");
-    
-    // Standardmäßig deaktiviert
-    addNewCarButton.title = "Bitte zuerst anmelden!";
-    addNewCarButton.disabled = true;
-    
-    let isLoggedIn = false;
-    
-    logInButton.addEventListener("click", function () {
-        // Hier sollte eine echte Login-Überprüfung erfolgen
-        //const loginSuccess = confirm("Login erfolgreich?"); // Simulierte Login-Bestätigung
-        let loginSuccess=false;
+  // Standardmäßig deaktiviert
+  addNewCarButton.title = "Bitte zuerst anmelden!";
+  addNewCarButton.disabled = true;
+
+  let isLoggedIn = false;
+  const Element = document.createElement("div");
+  Element.className = "car-listing";
+  elements.main.appendChild(Element);
+
+  loginButton.addEventListener("click", function () {
+    // Hier sollte eine echte Login-Überprüfung erfolgen
+    //const loginSuccess = confirm("Login erfolgreich?"); // Simulierte Login-Bestätigung
+    let loginSuccess = false;
+    let users = null;
     // Login function
-        function login(username, password) {
-            fetch('../assets/json/users.json')
-            .then(response => response.json())
-            .then(data => {
-                elements.data=data;
-                populateFilters(data);
-                displayCars(data);
-            })
-            .catch(error => console.error('Error loading the JSON file:', error));
-            // Find the user in the array
-            // car.attributes ["Marken"]
-            const user = users.find(user => user.attributes.userName === username);
-
-            // Check if the user exists and the password matches
-            if (user && user.attributes.userPassword === password) {
-                return "Login successful! Welcome, " + user.attributes.userName + "!";
-                loginSuccess=true;
-            } else {
-                return "Invalid username or password. Please try again.";
-            }
-        }    
-        if (loginSuccess) {
-            isLoggedIn = true;
-            addNewCarButton.disabled = false;
-            addNewCarButton.title = "Jetzt können Sie ein neues Auto hinzufügen!";
-            //logInButton.disabled = true;
+    fetch("../assets/json/users.json")
+      .then((response) => response.json())
+      .then((attribute) => {
+        users = attribute;
+        const user = users.find(user => user.attributes.userName === currentName.value);
+        if (user && user.attributes.userPassword === currentPassword.value) {
+          Element.innerText ="Login successful! Welcome, " + users["userName"] + "!";
+          loginSuccess = true;
+          elements.main.appendChild(Element);
+          loginSuccess = true;
+        } else {
+          Element.innerText = "Invalid username or password. Please try again.";
+          elements.main.appendChild(Element);
+          loginSuccess = false;
         }
-    });
-}
+        if (loginSuccess) {
+          isLoggedIn = true;
+          addNewCarButton.disabled = false;
+          addNewCarButton.title = "Jetzt können Sie ein neues Auto hinzufügen!";
+          //logInButton.disabled = true;
+        }
+
+      })
+     
+      .catch((error) => console.error("Error loading the JSON file:", error));
+
+    // Find the user in the array
+    //const user = elements.data.find(user => elements.data.attributes ["userName"] === username);
+
+    // Check if the user exists and the password matches
+  });
+};
 export default login;
